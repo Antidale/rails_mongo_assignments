08901edc2d99546b98f32071991357a8df81c2f7
Scott Wyngarden <swyngarden@gmail.com>
2018-06-14 12:44:54 -0700

    added destroy and contents methods, with find_nearest_place shell

--- practice/module_2_assignent/places/app/models/photo.rb
+++ practice/module_2_assignent/places/app/models/photo.rb
@@ -16,8 +16,28 @@ class Photo
     self.mongo_client.database.fs
   end
 
+  def self.all skip = 0, limit = 0
+    self.collection.find.skip(skip).limit(limit).collect { |doc| Photo.new(doc)}
+  end
+
+  def self.find id
+    doc = self.collection.find(_id: BSON::ObjectId.from_string(id)).first
+    doc.nil? ? nil : Photo.new(doc)
+  end
+
   def persisted?
     !@id.nil?
+  end
+
+  def contents
+    grid_file = self.class.collection.find_one(_id: BSON::ObjectId.from_string(id))
+
+    if grid_file
+      buffer = ""
+      grid_file.chunks.reduce([]) { | x , chunk | buffer << chunk.data.data }
+    end
+
+    return  buffer
   end
 
   def save
@@ -32,8 +52,14 @@ class Photo
     file = Mongo::Grid::File.new(@contents.read, description)
     @contents.rewind
     @id = self.class.collection.insert_one(file).to_s
-
   end
 
+  def destroy
+    self.class.collection.find(:_id => BSON::ObjectId.from_string(@id)).delete_one
+  end
+
+  def find_nearest_place max_distance
+
+  end
 
 end
