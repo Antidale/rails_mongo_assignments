a203a98a2c519c504702defbba926fb74ffa6cda
Scott Wyngarden <swyngarden@gmail.com>
2018-06-14 12:12:50 -0700

    downgrading EXIFR so it doens't throw uninitialized constant errors, implementing save

--- practice/module_2_assignent/places/app/models/photo.rb
+++ practice/module_2_assignent/places/app/models/photo.rb
@@ -2,15 +2,18 @@ class Photo
   attr_accessor :id, :location
   attr_writer :contents
 
+  def initialize params = {}
+    @id = params[:_id].nil? ? nil : params[:_id].to_s
+
+    @location = params[:metadata].nil? ? nil : Point.new(params[:metadata][:location])
+  end
 
   def self.mongo_client
     Mongoid::Clients.default
   end
 
-  def initialize params = {}
-    @id = params[:_id].nil? ? nil : params[:_id].to_s
+  def self.all
 
-    @location = params[:metadata].nil? ? nil : Point.new(params[:metadata][:location])
   end
 
   def persisted?
@@ -18,6 +21,19 @@ class Photo
   end
 
   def save
+    return if persisted?
+    coords = EXIFR::JPEG.new(@contents).gps
+    @location = Point.new(:lat => coords.latitude, :lng => coords.longitude)
+    @contents.rewind
+    description = {
+      :content_type => "image/jpeg",
+      :metadata => {:location => @location.to_hash }
+    }
+    file = Mongo::Grid::File.new(@contents.read, description)
+    @contents.rewind
+    @id = self.class.mongo_client.database.fs.insert_one(file).to_s
 
   end
+
+
 end
