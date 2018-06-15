2f8fe78e355aa2a64784a544260145ab9c4e5a76
Scott Wyngarden <swyngarden@gmail.com>
2018-06-14 19:20:57 -0700

    finished nearest_pace_id

--- practice/module_2_assignent/places/app/models/photo.rb
+++ practice/module_2_assignent/places/app/models/photo.rb
@@ -41,25 +41,47 @@ class Photo
   end
 
   def save
-    return if persisted?
-    coords = EXIFR::JPEG.new(@contents).gps
-    @location = Point.new(:lat => coords.latitude, :lng => coords.longitude)
-    @contents.rewind
-    description = {
-      :content_type => "image/jpeg",
-      :metadata => {:location => @location.to_hash }
-    }
-    file = Mongo::Grid::File.new(@contents.read, description)
-    @contents.rewind
-    @id = self.class.collection.insert_one(file).to_s
+    if persisted?
+      self.class.collection.find(:_id => BSON::ObjectId.from_string(@id)).update_one(:$set => {:metadata => {
+        :location => @location.to_hash,
+        :place => @place.to_hash}
+      })
+    else #New record
+      coords = EXIFR::JPEG.new(@contents).gps
+      @location = Point.new(:lat => coords.latitude, :lng => coords.longitude)
+      @contents.rewind
+      description = {
+        :content_type => "image/jpeg",
+        :metadata => {
+          :location => @location.to_hash,
+          :place => @place.to_hash
+        }
+      }
+      file = Mongo::Grid::File.new(@contents.read, description)
+      @contents.rewind
+      @id = self.class.collection.insert_one(file).to_s
+    end
   end
 
   def destroy
     self.class.collection.find(:_id => BSON::ObjectId.from_string(@id)).delete_one
   end
 
-  def find_nearest_place max_distance
-
+  def find_nearest_place_id max_distance
+    result = Place.near(@location, max_distance).limit(1).projection(:_id => 1).first
+    result ? result[:_id] : nil
   end
+
+  # def place
+  #   Place.find(:_id => BSON::ObjectId.from_string(@place))
+  # end
+
+  # def place=(value)
+  #   #accept BSON::ObjectId, string, or Place instance
+  #   @place = value.to_string if value.is_a? BSON::ObjectId
+  #   @place = value if value.is_a? String
+  #   @place = value.id if value.is_a? Place
+  # end
+
 
 end
