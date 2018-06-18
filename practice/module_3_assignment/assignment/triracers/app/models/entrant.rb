fdb24c0f31b512689607d92231209a8cf0125263
Scott Wyngarden <swyngarden@gmail.com>
2018-06-18 14:05:25 -0700

    1:1 embedded relationship where entrant has a copy of RaceRef

--- practice/module_3_assignment/assignment/triracers/app/models/entrant.rb
+++ practice/module_3_assignment/assignment/triracers/app/models/entrant.rb
@@ -3,6 +3,7 @@ class Entrant
   include Mongoid::Timestamps
   store_in collection: :results
   embeds_many :results, class_name: 'LegResult', after_add: :update_total
+  embeds_one :race, class_name: "RaceRef"
   field :bib, type: Integer
   field :secs, type: Float
   field :o, as: :overall, type: Placing
--- practice/module_3_assignment/assignment/triracers/app/models/race_ref.rb
+++ practice/module_3_assignment/assignment/triracers/app/models/race_ref.rb
@@ -2,4 +2,5 @@ class RaceRef
   include Mongoid::Document
   field :n, as: :name, type: String
   field :date, type: Date
+  embedded_in :entrant
 end
