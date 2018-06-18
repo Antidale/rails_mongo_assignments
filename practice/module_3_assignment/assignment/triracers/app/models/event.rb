193e9af4c1387e5b75d4837b85292309786635c3
Scott Wyngarden <swyngarden@gmail.com>
2018-06-17 19:53:54 -0700

    added instance methods for converting to meters or miles from expected possibilities

--- practice/module_3_assignment/assignment/triracers/app/models/event.rb
+++ practice/module_3_assignment/assignment/triracers/app/models/event.rb
@@ -1,7 +1,27 @@
 class Event
   include Mongoid::Document
-  field :o, type: Integer
-  field :n, type: String
-  field :d, type: Float
-  field :u, type: String
+  field :o, as: :order,  type: Integer
+  field :n, as: :name, type: String
+  field :d, as: :distance, type: Float
+  field :u, as: :units, type: String
+
+  def meters
+    case self.units
+    when "meters" then self.distance
+    when "miles" then self.distance * 1609.344
+    when "yards" then self.distance * 0.9144
+    when "kilometers" then self.distance * 1000
+    else nil
+    end
+  end
+
+  def miles
+    case self.units
+    when "meters" then self.distance * 0.000621371
+    when "miles" then self.distance
+    when "yards" then self.distance * 0.000568182
+    when "kilometers" then self.distance * 0.621371
+    else nil
+    end
+  end
 end
