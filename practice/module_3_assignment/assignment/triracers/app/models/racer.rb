70438ef56208a2465eccb97da40f7a2e46cd0d17
Scott Wyngarden <swyngarden@gmail.com>
2018-06-17 19:05:02 -0700

    added racer and racer_info classes

--- practice/module_3_assignment/assignment/triracers/app/models/racer.rb
+++ practice/module_3_assignment/assignment/triracers/app/models/racer.rb
@@ -1,4 +1,9 @@
 class Racer
   include Mongoid::Document
-  embeds_one :racer_info, as :locatable, class RacerInfo
+  embeds_one :info, as: :parent, class_name: 'RacerInfo', autobuild: true
+
+
+  before_create do |racer|
+    racer.info.id = racer.id
+  end
 end
--- practice/module_3_assignment/assignment/triracers/app/models/racer_info.rb
+++ practice/module_3_assignment/assignment/triracers/app/models/racer_info.rb
@@ -1,6 +1,6 @@
 class RacerInfo
   include Mongoid::Document
-  embedded_in :locatable, polymorphic: true
+  embedded_in :parent, polymorphic: true
   field :racer_id, as: :_id
   field :_id, default:->{racer_id}
   field :fn, as: :first_name, type: String
@@ -8,4 +8,9 @@ class RacerInfo
   field :g, as: :gender, type: String
   field :yr, as: :birth_year, type: Integer
   field :res, as: :residence, type: Address
+
+  validates :first_name, :last_name, :gender, :birth_year, presence: true
+  validates :gender, inclusion: { in: ['M', 'F'],
+    message: "%{value} must be either 'M' or 'F'"}
+  validates :birth_year, numericality: { less_than: Date.today.year }
 end
