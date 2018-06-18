c3ee6089ce0c99069898c1d0e1f368b6bdafe8f8
Scott Wyngarden <swyngarden@gmail.com>
2018-06-17 19:16:27 -0700

    added race class

---
+++ practice/module_3_assignment/assignment/triracers/app/models/race.rb
@@ -0,0 +1,7 @@
+class Race
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :n, as: :name, type: String
+  field :date, type: Date
+  field :loc, as: :location, type: Address
+end
