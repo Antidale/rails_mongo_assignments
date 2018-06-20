e4c9482a6867518cf9e156ae2343c2442ff512df
Scott Wyngarden <swyngarden@gmail.com>
2018-06-20 13:07:21 -0700

    flattening access to embedded racer/race/racer_info docs

--- practice/module_3_assignment/assignment/triracers/app/models/entrant.rb
+++ practice/module_3_assignment/assignment/triracers/app/models/entrant.rb
@@ -2,14 +2,43 @@ class Entrant
   include Mongoid::Document
   include Mongoid::Timestamps
   store_in collection: :results
+  #relationships
   embeds_many :results, class_name: 'LegResult', after_add: :update_total
-  embeds_one :race, class_name: "RaceRef"
-  embeds_one :racer, as: :parent, class_name: "RacerInfo"
+  embeds_one :race, class_name: "RaceRef", autobuild: true
+  embeds_one :racer, as: :parent, class_name: "RacerInfo", autobuild: true
+  #fields
   field :bib, type: Integer
   field :secs, type: Float
   field :o, as: :overall, type: Placing
   field :gender, type: Placing
   field :group, type: Placing
+  #flatten embedded document fields for cleaner UI design
+  delegate :first_name, :first_name=, to: :racer
+  delegate :last_name, :last_name=, to: :racer
+  delegate :gender, :gender=, to: :racer, prefix: "racer"
+  delegate :birth_year, :birth_year=, to: :racer
+  delegate :city, :city=, to: :racer
+  delegate :state, :state=, to: :racer
+  delegate :name, :name=, to: :race, prefix: "race"
+  delegate :date, :date=, to: :race, prefix: "race"
+
+  def overall_place
+    overall.place if overall
+  end
+
+  def gender_place
+    gender.place if gender
+  end
+
+  def group_place
+    group.place if group
+  end
+
+  def group_name
+    group.name if group
+  end
+
+
 
   def update_total result
     self[:secs] = results.reduce(0) { | total, result| total + result.secs}
--- practice/module_3_assignment/assignment/triracers/app/models/race.rb
+++ practice/module_3_assignment/assignment/triracers/app/models/race.rb
@@ -9,4 +9,49 @@ class Race
 
   scope :upcoming, -> {where(:date.gte => Date.current)}
   scope :past, -> {where(:date.lt => Date.current)}
+
+  def self.default
+    Race.new do |race|
+      DEFAULT_EVENTS.keys.each {|leg| race.send("#{leg}")}
+    end
+  end
+
+  ["city", "state"].each do |action|
+    #custom getter
+    define_method("#{action}") do
+      self.location ? self.location.send("#{action}") : nil
+    end
+
+    #custom setter
+    define_method("#{action}=") do |name|
+      object = self.location ||= Address.new
+      object.send("#{action}=", name)
+      self.location = object
+    end
+  end
+
+  DEFAULT_EVENTS = {
+    "swim" => {:order=> 0, :name=> "swim", :distance => 1, :units => "miles"},
+    "t1"   => {:order=> 1, :name=> "t1"},
+    "bike" => {:order=> 2, :name=> "bike", :distance=> 25, :units=> "miles" },
+    "t2"   => {:order=> 3, :name=> "t2"},
+    "run"  => {:order=> 4, :name=> "run", :distance=> 10, :units=>"kilometers"}
+  }
+
+  DEFAULT_EVENTS.keys.each do | name |
+    define_method("#{name}") do
+      event = events.select{|event| name == event.name}.first
+      event ||= events.build(DEFAULT_EVENTS["#{name}"])
+    end
+    ["order", "distance", "units"].each do | prop |
+      if DEFAULT_EVENTS["#{name}"][prop.to_sym]
+        define_method("#{name}_#{prop}") do
+          event = self.send("#{name}").send("#{prop}")
+        end
+        define_method("#{name}_#{prop}=") do |value|
+          event = self.send("#{name}").send("#{prop}=", value)
+        end
+      end
+    end
+  end
 end
